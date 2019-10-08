4efbb57180eef069a353b06e871797b15db26f54
Jay-68 <kelvinngari1995@gmail.com>
2019-10-08 12:17:54 +0300

    add an awesome comment in run file

--- app/run.py
+++ app/run.py
@@ -2,3 +2,4 @@ from app import app
 
 if __name__=='__main__':
   app.run(debug=True)
+# import the app instance and check if the script runs directly and then use the app.run() method to pass in the debug=True argument to allow us to run in debugger mode to track errors in our application.
