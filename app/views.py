93675436224cbddc76e22af1e63e7f4f74ebd7f9
Jay-68 <kelvinngari1995@gmail.com>
2019-10-08 15:14:47 +0300

    crreated variable block

--- app/views.py
+++ app/views.py
@@ -7,5 +7,8 @@ def index():
   '''
   View root page function that returns the index page and its data
   '''
-  return render_template('index.html')
+  message='Hello World Domination!'
+  # passed as an argument in the render_template..
+  # the message is a variable block used in the index
+  return render_template('index.html',message=message)
   # import render_template from flask - takes the name of the template as its first argument
