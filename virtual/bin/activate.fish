7b4c59f9383c37df18390e1771a43d637c4d8105
Jay-68 <kelvinngari1995@gmail.com>
2019-10-08 15:06:21 +0300

    fixed virtual error

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/moringa/James/Moringa_School_projects/watchlist/virtual"
+set -gx VIRTUAL_ENV "/home/moringa/Desktop/trial/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
