10450a238024f38117971a9c1b8073466c24161c
Jay-68 <kelvinngari1995@gmail.com>
2019-10-08 15:34:52 +0300

    created a dynamic route for movie.html

--- app/views.py
+++ app/views.py
@@ -12,3 +12,11 @@ def index():
   # the message is a variable block used in the index
   return render_template('index.html',message=message)
   # import render_template from flask - takes the name of the template as its first argument
+
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+  '''
+  view movie page functions that returns the movie details page and its data
+  '''
+  return render_template('movie.html',id=movie_id)
+  # add a new route with a movie function and a dynamic route <> which returns a response of a movie.html
