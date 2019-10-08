08100111686af6a1e558a498bbe425186c937170
Jay-68 <kelvinngari1995@gmail.com>
2019-10-08 16:01:44 +0300

    set up app configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from .config import DevConfig
 
 # initializing the application
 app=Flask(__name__)
+
+# setting up configuration
+app.config.from_object(DevConfig)
+
+# to make the application use the new configurations we import the DevConfig subclass in __init__.py.
+# we then use the app.config.from_object() method to set up configuration and pass the DevConfig as our arguments
+
 from app import views
 
 # import flask class from flask module and use it to create an app instance of the class flask and pass in the variable __name__
