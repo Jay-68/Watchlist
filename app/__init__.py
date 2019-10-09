f978fd71c1398e89b7bfdb42d776d711a0008e8d
Jay-68 <kelvinngari1995@gmail.com>
2019-10-09 10:01:26 +0300

    create a new config file and connect it to the app

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from .config import DevConfig
 
 # initializing the application
-app=Flask(__name__)
+app=Flask(__name__,instance_relative_config=True)
 
 # setting up configuration
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 # to make the application use the new configurations we import the DevConfig subclass in __init__.py.
 # we then use the app.config.from_object() method to set up configuration and pass the DevConfig as our arguments
